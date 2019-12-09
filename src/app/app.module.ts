bdf83e156905ef91afe208044de11bfbaed78b3b
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-12-09 03:45:41 +0300

    created services file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
