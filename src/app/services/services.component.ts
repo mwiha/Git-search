6ef84ea4af1e58f657c3779c5dfd6caa76f304a2
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-12-09 04:43:17 +0300

    inserted injectables and links

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  @Injectable()
+export class ServicesComponent implements OnInit { 
+
+        private username :string;
+        private clientid='c356ecdee180dd53ef95';
+        private clientsecret='acf10bc9294ccdc784414d7fb57f2fcdfb92f4ff';
+
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
